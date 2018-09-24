74919e2498f79010e88a2fea7b33e57b40c6d55b
Ppridorozhny <Peter35128@gmail.com>
2018-09-24 16:54:51 +0300

    added groups

--- src/impl/Lab1.java
+++ src/impl/Lab1.java
@@ -11,6 +11,8 @@ public class Lab1 implements Processable {
     private Map<Integer, String[]> allValues;
     private Map<Integer, Integer[]> newMatrixValues;
     private Set uniqueVal;
+    private Map<Integer, Set> groups;
+    private Set<Integer> usedRowNumbers;
 
     public Lab1(Start main) {
         this.main = main;
@@ -25,6 +27,8 @@ public class Lab1 implements Processable {
         printMap(allValues);
         uniqueValues();
         goTable(uniqueVal.size());
+        createGroups();
+        System.out.println(1);
     }
 
     private void parseRows() {
@@ -49,7 +53,7 @@ public class Lab1 implements Processable {
         }
     }
 
-    private void goTable(int amountUniqeValues){
+    private void goTable(int amountUniqueValues){
 
         String[] values;
         String[] values2;
@@ -67,7 +71,7 @@ public class Lab1 implements Processable {
                 countOfColizion = checkCollisions(values, values2);
 
                 allStringValues = values.length + values2.length;
-                newMatrixValue[j] = amountUniqeValues - (allStringValues - (2 * countOfColizion));
+                newMatrixValue[j] = amountUniqueValues - (allStringValues - (2 * countOfColizion));
             }
                 newMatrixValues.put(i, newMatrixValue);
                 newMatrixValue = new Integer [main.getRankOfTheMatrix()];
@@ -90,6 +94,85 @@ public class Lab1 implements Processable {
         for (Integer value : allValues.keySet()){
             uniqueVal.addAll(Arrays.asList(allValues.get(value)));
         }
+    }
+
+    private int findMaxValue(int maxValue) {
+        int max = 0;
+
+        for (Integer value : newMatrixValues.keySet()){
+            for (Integer val : newMatrixValues.get(value)) {
+                if (val > max && val < maxValue) {
+                    max = val;
+                }
+            }
+        }
+
+        return max;
+    }
+
+    private void getRidOfNull() {
+        Integer[] array;
+        for (Integer value : newMatrixValues.keySet()) {
+            array = newMatrixValues.get(value);
+            for (int i = 0; i < array.length; ++i) {
+                if (array[i] == null) {
+                    array[i] = 0;
+                }
+            }
+        }
+    }
+
+    private boolean createGroupSet(int index, int currentMax) {
+
+        boolean added = true;
+
+        Set<Integer> set = new HashSet<>();
+        Integer[] array;
+        List<Integer> rows;
+
+        for (Integer value : newMatrixValues.keySet()) {
+            array = newMatrixValues.get(value);
+            for (int i = 0; i < array.length; ++i) {
+                rows = Arrays.asList(value, i);
+                if (array[i] == currentMax &&
+                        !(usedRowNumbers.contains(i) || usedRowNumbers.contains(value)) ) {
+                    set.addAll(rows);
+                }
+            }
+        }
+
+        if (set.size() != 0) {
+            groups.put(index, set);
+            usedRowNumbers.addAll(set);
+            added = false;
+        }
+
+        return added;
+    }
+
+    private void createGroups() {
+        int i;
+        int max = uniqueVal.size();
+        groups = new HashMap<>();
+        usedRowNumbers = new HashSet<>();
+        Set<Integer> lastElementSet = new HashSet<>();
+
+        getRidOfNull();
+
+        for (i = 0; max != 0;) {
+            max = findMaxValue(max);
+            if (createGroupSet(i, max)) {
+                ++i;
+            }
+        }
+
+        for (int j = 0; j < main.getRankOfTheMatrix(); ++j) {
+            if (!usedRowNumbers.contains(j)) {
+                lastElementSet.add(j);
+                groups.put(i, lastElementSet);
+            }
+        }
+
     }
 
 }
