5344ab529b1907f8977a83a4b4d00b85410302a5
Ppridorozhny <Peter35128@gmail.com>
2018-11-11 14:43:02 +0200

    created relations object

---
+++ src/Entities/Relation.java
@@ -0,0 +1,27 @@
+package Entities;
+
+public class Relation {
+    private int amountOfInputRelations;
+    private int amountOfOutputRelations;
+
+    public Relation(int amountOfInputRelations, int amountOfOutputRelations) {
+        this.amountOfInputRelations = amountOfInputRelations;
+        this.amountOfOutputRelations = amountOfOutputRelations;
+    }
+
+    public int getAmountOfInputRelations() {
+        return amountOfInputRelations;
+    }
+
+    public void setAmountOfInputRelations(int amountOfInputRelations) {
+        this.amountOfInputRelations = amountOfInputRelations;
+    }
+
+    public int getAmountOfOutputRelations() {
+        return amountOfOutputRelations;
+    }
+
+    public void setAmountOfOutputRelations(int amountOfOutputRelations) {
+        this.amountOfOutputRelations = amountOfOutputRelations;
+    }
+}
